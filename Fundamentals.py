6d26ce7b80a450f46a842c291f0eecfad0fb6a6d
💝Manu 🙋‍♀️ <vallelamanoja@gmail.com>
2026-06-23 17:29:21 +0530

    USED PRINT COMMAND
    Using print command .. 🤩🤩created a paragraph introducing myself

---
+++ Fundamentals.py
@@ -0,0 +1,20 @@
+name=input("What is your name :")
+age=input("Enter you age:")
+place=input("Where do you live:")
+country=input("In which country do you live in :")
+goal=input("What is your goal: ")
+hobby=input("What is your hobby:")
+nick_name=input("What is your nick name :")
+food=input("Your favourite food :")
+love=input("whom do you love the most :")
+
+print(
+    "Hello, I am",name,"."
+    "I am ",age, "years old.",
+     "I live in",place, "in", country,"."
+     "My goal is to", goal,"."
+     "My hobby is" ,hobby,"."
+     "My nick name is",nick_name,"."
+     "My favourite food is ",food,"."
+     "I love my ", love, "a lot."
+     ) 
