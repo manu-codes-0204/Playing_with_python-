91bffba20fc460ac213ce5d2b3efe18923e13662
💝Manu 🙋‍♀️ <vallelamanoja@gmail.com>
2026-06-23 11:14:53 +0530

    Data types and conversions ☺️
    Introduced 😎data types and the conversions of one into other .. 👌

---
+++ Play_2.py
@@ -0,0 +1,29 @@
+n=int(9)
+print(n)
+print("The type of n is", type(n))
+m=float(9.9)
+print(m)
+print("The type of m is", type(m))
+l="MANU"
+print(l)
+print("The type of l is" ,type(l))
+k=True
+print(k)
+print("The type of k is",type(k))
+
+n=float(9)
+print(n,type(n))
+
+m=int(9.9)
+print(m,type(m))
+
+l=str(9.9)
+print(l,type(l))
+
+a=bool(10)
+b=bool(0)
+c=bool(-10)
+
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
